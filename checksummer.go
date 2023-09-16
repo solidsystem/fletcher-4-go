e912728dd1dda5620b518dcc78606de6079b93c7
Jostein Stuhaug <jostein@solidsystem.no>
2023-09-16 21:08:21 +0200

    Implementation and unit test of fletcher-4 checksum lib added.

---
+++ checksummer.go
@@ -0,0 +1,96 @@
+package fletcher4 // import go.solidsystem.no/fletcher4
+
+import (
+	"encoding/binary"
+	"fmt"
+	"hash"
+)
+
+// Extension of common Hash interface to easily get 4 computed checksum words
+type Fletcher64x4 interface {
+	hash.Hash
+	Sum64x4() [4]uint64
+}
+
+// The size of a fletcher4 checksum in bytes
+const Size = 32
+
+// Must be the same as size of uint32 with the current implementation. Not entirely sure it's the correct value to return as blocksize, but think so.
+const BlockSize = 4
+
+// digest represents the partial evaluation of a fletcher4 checksum.
+type digest [4]uint64
+
+func (d *digest) Reset() {
+	*d = [4]uint64{0, 0, 0, 0}
+}
+
+// New returns a new Fletcher64x4 (hash.Hash) computing the fletcher4 checksum.
+func New() Fletcher64x4 {
+	d := new(digest)
+	d.Reset()
+	return d
+}
+
+func (d *digest) Size() int { return Size }
+
+func (d *digest) BlockSize() int {
+	return BlockSize
+}
+
+// Add p to the running checksum d.
+func update(dig digest, p []byte) digest {
+	a := dig[0]
+	b := dig[1]
+	c := dig[2]
+	d := dig[3]
+
+	// Incase input is not padded to 4 bytes
+	if len(p)%BlockSize != 0 {
+		panic(fmt.Sprintf("Write to Fletcher64x4 checksummer must be a multiple of %v bytes.", BlockSize))
+	}
+
+	/*  This fix was deactivated, not sure this would be correct to do, if repeated writes to the checksummer are done with too few
+		bytes the checksum would probably be wrong at the end. All writes must be a multiple of BlockSize, else we panic
+	var p []byte
+	if remainder := len(p) % BlockSize; remainder != 0 {
+		p = make([]byte, len(p)+remainder)
+		copy(p, add)
+	} else {
+		p = add
+	}
+	*/
+
+	for i := 0; i < len(p); i += BlockSize {
+		a += uint64(binary.LittleEndian.Uint32(p[i : i+BlockSize]))
+		b += a
+		c += b
+		d += c
+	}
+
+	return digest{a, b, c, d}
+}
+
+func (d *digest) Write(p []byte) (n int, err error) {
+	*d = update(*d, p)
+	return len(p), nil
+}
+
+func (d *digest) Sum(in []byte) []byte {
+	add := make([]byte, 8)
+	binary.LittleEndian.PutUint64(add, d[0])
+	ret := append(in, add...)
+	binary.LittleEndian.PutUint64(add, d[1])
+	ret = append(ret, add...)
+	binary.LittleEndian.PutUint64(add, d[2])
+	ret = append(ret, add...)
+	binary.LittleEndian.PutUint64(add, d[3])
+	ret = append(ret, add...)
+
+	return ret
+}
+
+// Returns the current checksum
+func (d *digest) Sum64x4() [4]uint64 {
+	return [4]uint64(*d)
+}
---
+++ checksummer_test.go
@@ -0,0 +1,61 @@
+package fletcher4
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+type hexRes [4]string
+
+func compare(t *testing.T, leadTxt string, exp hexRes, got [4]uint64) {
+	gotHex := fmt.Sprintf("%x/%x/%x/%x", got[0], got[1], got[2], got[3])
+	expHex := fmt.Sprintf("%v/%v/%v/%v", exp[0], exp[1], exp[2], exp[3])
+
+	if expHex != gotHex {
+		t.Errorf("%v:\nexpected\t%v,\ngot\t\t%v", leadTxt, expHex, gotHex)
+	}
+}
+
+// Test that writing 4 bytes fills all checksum result uints with the same 4 bytes
+func TestChecksummer1(t *testing.T) {
+	inp1 := []byte{1, 2, 3, 4}
+	exp1 := hexRes{"4030201", "4030201", "4030201", "4030201"}
+
+	checksummer := New()
+	checksummer.Write(inp1)
+	res1 := checksummer.Sum64x4()
+	compare(t, "Checksum test 1, 4 bytes failed", exp1, res1)
+
+}
+
+// Test that writing 8 bytes and then 4 more gives correct result.
+// Also test that Sum return correct result
+func TestChecksummer2(t *testing.T) {
+	inp1 := []byte{1, 2, 3, 4, 5, 6, 7, 8}
+	exp1 := hexRes{"c0a0806", "100d0a07", "14100c08", "18130e09"}
+
+	checksummer := New()
+	checksummer.Write(inp1)
+	res1 := checksummer.Sum64x4()
+	compare(t, "Checksum test 2, 8 bytes written failed", exp1, res1)
+
+	inp2 := []byte{2, 4, 6, 8}
+	exp2 := hexRes{"14100c08", "241d160f", "382d2217", "50403020"}
+	checksummer.Write(inp2)
+	res2 := checksummer.Sum64x4()
+	compare(t, "Checksum test 2, 12 bytes written failed", exp2, res2)
+
+	var sum []byte
+	sum = checksummer.Sum(sum)
+	expSum := []byte{8, 12, 16, 20, 0, 0, 0, 0, 15, 22, 29, 36, 0, 0, 0, 0, 23, 34, 45, 56, 0, 0, 0, 0, 32, 48, 64, 80, 0, 0, 0, 0}
+	if !bytes.Equal(sum, expSum) {
+		t.Errorf("Checksum Sum method call 1 returned wrong result.\nExpected %x,\ngot: %x)", sum, expSum)
+	}
+	// Test that checksummer.Sum appends correctly
+	sum = checksummer.Sum(sum)
+	expSum2 := append(expSum, expSum...)
+	if !bytes.Equal(sum, expSum2) {
+		t.Errorf("Checksum Sum method call 2 returned wrong result.\nExpected %x,\ngot: %x)", sum, expSum2)
+	}
+}
